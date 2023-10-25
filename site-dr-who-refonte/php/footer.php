b55643a3e37ec9bc6d95acee43a5353e6d873ca9
Christophe Porte <Christophe.PORTE@etu.uca.fr>
2023-10-25 14:49:30 +0200

    new repo

---
+++ site-dr-who-refonte/php/footer.php
@@ -0,0 +1,5 @@
+<hr/>
+<footer>
+	<p>Si vous rencontrez le moindre soucis sur notre site, contacter nous l'adresse ci-dessous:</p>
+	<p>porte17christophe69@gmail.com</p>
+</footer>
---
+++ site-dr-who-refonte/php/reseaux.php
@@ -0,0 +1,10 @@
+<aside id="aside1">
+	<div id='res'>
+		<a href='#'>reseaux</a>
+		<ul id="select_aside">
+			<li class="li1 reseaux"><a href="">Facebook</a></li>
+			<li class="li1 reseaux"><a href="">Snapchat</a></li>
+			<li class="li1 reseaux"><a href="">Twitter</a></li>
+		</ul>
+	</div>
+</aside>
