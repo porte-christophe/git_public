28480c511c06f0f9d01eb96d76860b4f82710d43
porte-christophe <148970363+porte-christophe@users.noreply.github.com>
2023-10-26 16:28:11 +0200

    Delete site-dr-who-refonte directory

--- site-dr-who-refonte/config/connection.php
+++
@@ -1,3 +0,0 @@
-<?php
-	$dbh = new PDO('mysql:host=localhost;dbname=drwho', "bdlogin", "nigoldb");
-?>
--- site-dr-who-refonte/php/footer.php
+++
@@ -1,5 +0,0 @@
-<hr/>
-<footer>
-	<p>Si vous rencontrez le moindre soucis sur notre site, contacter nous l'adresse ci-dessous:</p>
-	<p>porte17christophe69@gmail.com</p>
-</footer>
