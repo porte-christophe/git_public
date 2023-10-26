148feecb5c69fe6869dd04dad7f181902196f519
Christophe Porte <Christophe.PORTE@etu.uca.fr>
2023-10-26 16:31:07 +0200

    new repo

---
+++ Drwho_actu2.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="fr" > 
+	<head>
+			<meta charset="utf-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<title>Drwho actu</title>
+			<link rel="preconnect" href="https://fonts.googleapis.com">
+			<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+			<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+			<link 	href="css/style_general.css" type="text/css"
+					rel="stylesheet" media="screen"/> 
+			<link 	rel="icon" type="image/jpg" href="images/logo.jpg" />
+	</head>  
+	<body> 
+		<?php
+			include 'php/header.php';
+			include 'php/reseaux.php';
+		?>
+
+		
+		<h2>Présentation</h2>
+		<p2><mark><em>Le Docteur</em></mark> est un/une <strong>seigneur/dame du temps</strong>,sillonant l'univers avec ses compagnons(gnes) à bord du <mark><em>T.A.R.D.I.S</em></mark> tellement agé que (je cite) <strong><em>"je ne sais même plus si je mens sur mon âge"</em></strong>.<br/>
+		Il/Elle passe sont temps à sauver le monde, à combattre ses ennemis et à rétablir la justice,suivant une promesse <strong><em>"never be cruel, never be cowardly"</em></strong> (faite à lui/elle-même).<br/>
+		</p2>
+		
+		<a href= "https://fr.wikipedia.org/wiki/Doctor_Who">
+		<img src= "image/drwho3.jpg" width="300px", title="Lien wiki"/>
+		</a>
+		
+		<footer>
+			<p>Si vous rencontrez le moindre soucis sur notre site, contacter nous l'adresse ci-dessous:</p>
+			<p>porte17christophe69@gmail.com</p>
+		</footer>
+	</body>
+</html>
